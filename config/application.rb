728c3a17c28bb96c10b1c87deec4c842633567a8
Jesse Scott <jesse.c.scott@gmail.com>
2012-10-04 15:09:26 -0700

    Created boilerplate spec files for existing models and controllers.
    Also tweaked the application configration to no longer load test-unit, action-mailer, or active-resource since we aren't using those.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -19,6 +25,8 @@ module Newton
       g.test_framework :rspec, :views => false, :fixture => true
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+    
+    config.generators.javascripts = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
