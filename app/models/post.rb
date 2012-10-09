e1237ede8a25257f7b20a7824f2391add7e6e48e
Jesse Scott <jesse.c.scott@gmail.com>
2012-10-09 15:31:29 -0700

    Added Post model tests.
    Fixed a bug in the "published_in_year_and_month" Post scope uncovered by testing.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,14 @@ class Post < ActiveRecord::Base
   attr_accessor :html
 
   validates :title, :slug, :post_type, :presence => true
-  validates :body, :presence => { :message => "%{value} can't be blank for published posts" }, :if => "published_at.present?"
-  validates :slug, :format => { :with => /^[a-z0-9\-]*$/, :message => "%{value} contains invalid characters for a post slug" }
-  validates :post_type, :inclusion => { :in => %w(link prose picture video), :message => "%{value} is not a valid post type" }
+  validates :body, :presence => { :message => "can't be blank for published posts" }, :if => "published_at.present?"
+  validates :slug, :format => { :with => /^[a-z0-9\-]*$/, :message => "contains invalid characters for a post slug" }
+  validates :post_type, :inclusion => { :in => %w(link prose picture video), :message => "is not a valid post type" }
   
   scope :draft, where('published_at IS NULL')
   scope :published, where('published_at IS NOT NULL')
 
-  scope :published_in_year_and_month, lambda { |year, month| where("published_at IS NOT NULL AND published_at BETWEEN datetime('#{year}-#{month}-01 00:00:00') AND datetime('#{year}-#{month}-01 00:00:00','start of month','+1 month','-0.001 second')") }
+  scope :published_in_year_and_month, lambda { |year, month| where("published_at IS NOT NULL AND published_at BETWEEN datetime('#{year}-#{"%02d" % month}-01 00:00:00') AND datetime('#{year}-#{"%02d" % month}-01 00:00:00','start of month','+1 month','-0.001 second')") }
   
   def published?
     self.published_at.present?
