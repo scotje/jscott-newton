43526cf8ae73394daaf4b0bd48ec6ee30cf2bacc
Jesse Scott <jesse.c.scott@gmail.com>
2012-10-09 19:24:02 -0700

    Cleaned up routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Newton::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-  
   namespace :admin do 
     resources :posts
     resources :drafts, :controller => "posts", :draft => true
